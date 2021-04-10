e6dd12392e20a5fb4ea5b0554990a3ff08654e80
Kuass <kua@kakao.com>
2021-04-10 16:21:56 +0900

    프록시 확인 방법

--- src/main/java/kr/kua/JpaMain.java
+++ src/main/java/kr/kua/JpaMain.java
@@ -1,9 +1,8 @@
 package kr.kua;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -24,17 +23,20 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member redMember = em.getReference(Member.class, member.getId());
-            System.out.println("member = " + redMember.getClass());
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("member = " + refMember.getClass());
 
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("reference = " + findMember.getClass());
+            refMember.getUsername();
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember)); // 프록시 인스턴스의 초기화 여부 확인
 
-            System.out.println("a == a: " + (redMember == findMember)); // true
+            System.out.println("refMember.getClass = " + refMember.getClass()); // 프록시 클래스 확인 방법
+
+            Hibernate.initialize(refMember); // 강제 초기화
 
             tx.commit();
         }catch(Exception e) {
             tx.rollback();
+            e.printStackTrace();
         }finally{
             em.close();
         }
