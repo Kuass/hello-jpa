1d8bd566cfcbd4091f47d36f445489d7435df446
Kua <kua@kakao.com>
2021-04-11 19:10:07 +0900

    영속성 전이(CASCADE) 저장 영관관계 매핑과는 아무 관계가 없음 편리함만 제공

--- src/main/java/kr/kua/Parent.java
+++ src/main/java/kr/kua/Parent.java
@@ -1,9 +1,6 @@
 package kr.kua;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +12,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL) // ALL or PERSIST
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
